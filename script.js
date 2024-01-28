5530ac0c616c8286046a8f2c4c19b3c36678e695
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2024-01-29 00:44:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function mincost(arr)
 //write your code here
 // return the min cost
   
-	for(let i;i<arr.length;i++){
+	for(let 0;i<arr.length;i++){
 		if(arr.length<2) return arr[0];
 		let first=100000; let second=100000;
 		let ind1=0, ind2=1;
