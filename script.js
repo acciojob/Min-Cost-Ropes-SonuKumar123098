c31455c2f5a2113a63f91f23bb6e5b1896afb8e0
SonuKumar123098 <112372023+SonuKumar123098@users.noreply.github.com>
2024-01-31 21:12:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,29 +2,17 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  let sum=0;
-	for(let i=0;i<arr.length;i++){
-		if(arr.length<2) return sum;
-		let first=100000; let second=100000;
-		let ind1=0, ind2=1;
-		for(let j in arr){
-			if(first>arr[j]){
-				first=arr[j];
-				ind1=j;
-			}
-		}
-
-		for(let j in arr){
-			if(second>arr[j] && ind1!=j){
-				second=arr[j];
-				ind2=j;
-			}
-		}
-		arr[ind2]=arr[ind1]+arr[ind2];
-		sum+=arr[ind2];
-		arr.splice(ind1,1);
+  let totalCost=0;
+	
+	while(arr.length>=2){
+		arr.sort((a,b)=> a-b)
+		let cost=arr[0]+arr[1];
+		arr.shift();
+		arr.shift();
+		arr.push(cost);
+		totalCost+=cost;
 	}
-	return sum;
+	return totalCost;
 }
 
 module.exports=mincost;
